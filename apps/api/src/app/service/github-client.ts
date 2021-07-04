c3cc2f5750a708627431fd9d5bc58281dee7810b
ehpankaj <ehpankaj@gmail.com>
2021-07-04 16:23:23 +0530

    added pagination support

--- apps/api/src/app/service/github-client.ts
+++ apps/api/src/app/service/github-client.ts
@@ -7,11 +7,23 @@ export class GitHubClient {
   constructor(private readonly octokit: Octokit) {}
 
   async getAllContributors(repository: Repository): Promise<Contributor[]> {
-    const { data } = await this.octokit.repos.listContributors({
-      owner: repository.owner,
-      repo: repository.repo,
-      per_page: 100,
-    });
-    return data as Contributor[];
+    let finalData: any[] = [];
+    let lastPageReached = false;
+    let pageCounter = 1;
+    while (!lastPageReached) {
+      const { data } = await this.octokit.repos.listContributors({
+        owner: repository.owner,
+        repo: repository.repo,
+        per_page: 100,
+        page: pageCounter,
+      });
+      finalData = [...finalData, ...data];
+      pageCounter += 1;
+      if (data.length == 0) {
+        lastPageReached = true;
+        pageCounter = 1;
+      }
+    }
+    return finalData as Contributor[];
   }
 }
